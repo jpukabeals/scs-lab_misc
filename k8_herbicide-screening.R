d96d3000e80ff2daa55177635d30e0b18bd83d3b
jpukabeals <77247525+jpukabeals@users.noreply.github.com>
2022-04-13 10:01:11 -0500

    prep for a screening experiment

---
+++ k8_herbicide-screening.R
@@ -0,0 +1,22 @@
+library(agricolae)
+
+
+# plot dimensions calcs ---------------------------------------------------
+
+# bicycle sprayer can do 80", 120" and 160"
+# 80" is easiest
+
+80/12
+# Spray area is 6.6'
+# round up to plots at 6'?
+
+# kernza at 12" row spacing
+# 6 rows of kernza 
+6*12
+
+
+
+# plots are 6.6' wide
+# plots 10' long, keep them smallish
+# that's 
+
