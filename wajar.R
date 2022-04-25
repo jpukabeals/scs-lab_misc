bc35fad41acc2ae5a9a28d8a57f21c74acd36eca
Jesse Puka-Beals <pukab001@umn.edu>
2022-04-24 21:10:59 -0500

    added dummy data as an example of what data could be created and how it would be analyzed

--- wajar.R
+++ wajar.R
@@ -70,4 +70,46 @@ agricolae::design.split(
 
 
 
+# generate dummy data -----------------------------------------------------
 
+
+legacy_cropping_system <- c("corn-soy-oat", "alfalfa-alfalfa-alfalfa")
+crop <- c("kernza-monoculture", "kernza-alfalfa-biculture")
+
+agricolae::design.split(
+  trt1 = legacy_cropping_system,
+  trt2 = crop,
+  r = 4,
+  design = "rcbd",
+  serie = 0,
+  seed = 314,
+  kinds = "Super-Duper",
+  first = T,
+  randomization = T
+) %>%
+  .$book %>% 
+  mutate(rep = rep(seq(1:4),4)) %>% 
+  mutate(experimental_unit = paste(block,rep,sep = "0"),
+         .before = plots) %>% 
+  dplyr::select(-c(rep,plots,splots,block)) -> df
+
+# generate yield data
+# groups of 4
+set.seed(314)
+rnorm(4, 450, 80) %>%  #alfalfa-intercrop
+  c(.,
+    rnorm(4, 500, 100)) %>%  #alfalfa-mono
+  c(.,
+    rnorm(4, 440, 50)) %>% #bau-intercrop
+  c(.,
+    rnorm(4, 480, 70) #bau-mono
+    ) -> yield_yr1
+
+
+df %>% 
+  arrange(legacy_cropping_system, crop) %>% 
+  mutate(yield_yr1 = yield_yr1) %>% 
+  ggplot(aes(crop,yield_yr1,
+             # col=legacy_cropping_system
+             )) +
+  stat_summary()
