15f9a895c8eb3479c4ff04426b81fee13ff62ed4
jpukabeals <pukab001@umn.edu>
2022-09-20 15:54:01 -0500

    NIR for legacynet mold test

---
+++ nir_legacynet_mold-test.R
@@ -0,0 +1,33 @@
+
+# Intro -------------------------------------------------------------------
+
+# We took subsamples from plots after our last forage harvest (Sep2022).
+# we dried these samples in paper bags at 140F, but they molded
+# to what extent does the mold impact the NIR predcitions?
+
+# we randomly selected 3 bags that contained rotted biomass
+# on the top and edges of the forage mass the sorghum sudangrass had not molded
+# in the center there was mold
+# in between the edges and middle there was a mixture of mold
+
+# There are 3 levels of mold. 
+
+# 1 = no mold. These were the dried green leaves at edge of sample that dried down
+# fast and did not mold
+
+# 2 = mixture of mold and no mold. If we had left these samples in the oven for weeks, the
+# sample would've eventually dried and the center would've been blacker in color
+# and the outside greener. I would've just grabbed a mix of these samples, this
+# is what 2 represents
+
+# 3 = mold. Sample was taken from center. Dark in color
+
+# There are 3 plots, randomly selected. All had been dried for 1 week at 140F
+# and shuffled a number of times and still molded
+
+
+# Import data -------------------------------------------------------------
+
+read.csv("nir_legacynet_20Sep2022.csv")
+
+
